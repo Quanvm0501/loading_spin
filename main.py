a17216bf13a2735a32497561c30caec925934d6a
Vu Minh Quan <92785316+Quanvm0501@users.noreply.github.com>
2024-04-12 20:08:00 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import loading_text
+def loading_spin(text: str, times):
+	from time import sleep as TIME_SLEEP
+    for _ in range(times_loop):
+        for tmp1 in ["\\", "|", "/", "-"]:
+            print(text, tmp1, end="\r")
+            TIME_SLEEP(0.1)
+    print(text,"\r", end = " ")
+    print(text,"\r", end = "")
