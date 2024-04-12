137e5235766f360e9a67e0a0dc403b2c28becaf0
Vu Minh Quan <92785316+Quanvm0501@users.noreply.github.com>
2024-04-12 20:32:55 +0700

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,20 @@
+class Error(Exception):
+	pass
+
+class Loading_Failed(Error):
+	pass
+
+def loading_spin(text: str, times):
+	from time import sleep as TIME_SLEEP
+	for _ in range(times_loop):
+		for tmp1 in ["\\", "|", "/", "-"]:
+			print(text, tmp1, end="\r")
+			TIME_SLEEP(0.1)
+	print(text,"\r", end = " ")
+	print(text,"\r", end = "")
+	return True
+
+if __name__ == "__main__":
+	tmp = loading_spin("Text", 10)
+	if tmp == True: print("Finished!")
+	else: raise Loading_Failed("Loading Spin Test Failed!")
